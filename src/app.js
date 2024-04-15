360d5fcdea720edc5b9b2dbb7d1a4d6f38d87dc4
NadegeRodier <129798006+NadegeRodier@users.noreply.github.com>
2024-04-15 18:20:39 -0500

    Coded navlinks so that class also changes on scroll

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section");
 
 function makeActive(currentLink) {
   navLinks.forEach((link) => {
@@ -11,4 +12,25 @@ navLinks.forEach((link) => {
   link.addEventListener("click", function () {
     makeActive(this);
   });
+});
+
+const options = {
+  threshold: 0.7, // Trigger when section is 50% visible
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const targetId = entry.target.getAttribute("id");
+      const currentNavLink = document.querySelector(
+        `.nav-link[href="#${targetId}"]`
+      );
+      navLinks.forEach((link) => link.classList.remove("nav-link-active"));
+      currentNavLink.classList.add("nav-link-active");
+    }
+  });
+}, options);
+
+sections.forEach((section) => {
+  observer.observe(section);
 });
