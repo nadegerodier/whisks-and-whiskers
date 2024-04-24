e669b53a8ce1ccf9e2644c90a434e5f871d69027
NadegeRodier <129798006+NadegeRodier@users.noreply.github.com>
2024-04-23 19:10:24 -0500

    Coded interaction to add items to the cart

--- src/app.js
+++ src/app.js
@@ -64,6 +64,7 @@ const cartSection = document.querySelector("#cart");
 const cartArea = document.querySelector(".cart-area");
 const shoppingCartIcon = document.querySelectorAll(".shopping-cart");
 const cartExitIcon = document.querySelector("#exit-cart");
+const emptyCartBtn = document.querySelector(".btn-empty-cart");
 
 shoppingCartIcon.forEach((cartIcon) => {
   cartIcon.addEventListener("click", function () {
@@ -85,11 +86,85 @@ cartExitIcon.addEventListener("click", function () {
 
 document.addEventListener("click", function (event) {
   const target = event.target;
-  if (!cartArea.contains(target) && target !== shoppingCartIcon[0]) {
+  if (
+    target === emptyCartBtn ||
+    (!cartArea.contains(target) && target !== shoppingCartIcon[0])
+  ) {
     cartArea.style.transform = "translateX(30px)";
     cartSection.style.opacity = "0";
     setTimeout(function () {
       cartSection.style.display = "none";
     }, 500);
   }
+});
+
+const bookBtn = document.querySelectorAll(".btn-book");
+const numberOfParticipants = document.querySelector(".participants-quantity");
+const bakingClassName = document.querySelector(".class-name");
+const classImage = document.querySelector(".class-img");
+const unitCost = document.querySelector(".unit-cost");
+const cartContentElement = document.querySelector(".cart-content");
+const cartFooter = document.querySelector(".cart-footer");
+let cartItems = [];
+
+function updateCart() {
+  if (cartItems.length === 0) {
+    cartContentElement.innerHTML = `<div class="empty-cart d-flex flex-column justify-content-center align-items-center">
+          <div class="empty-cart-message">
+            Your shopping cart is empty
+          </div>
+          <a href="#baking-classes" class="btn btn-empty-cart">
+            Shop our classes
+          </a>
+        </div>`;
+  } else {
+    cartContentElement.innerHTML = cartItems.join("");
+  }
+}
+
+function saveCartToLocalStorage() {
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+}
+
+bookBtn.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    cartFooter.classList.remove("d-none");
+    const price = unitCost.innerHTML;
+    let quantity = numberOfParticipants.value;
+    let priceTotal = price * quantity;
+    let newItem = `<div class="cart-item pt-4 pb-4">
+        <div class="row">
+          <div class="col-3">
+            <img
+              src=${classImage.src}
+              alt="baking class preview"
+              class="img-fluid rounded"
+            />
+          </div>
+          <div class="col-6 ps-0">
+            <h5 class="pt-1">${bakingClassName.innerHTML}</h5>
+            <div class="pt-2">
+              Quantity:
+              <input
+                class="item-quantity text-center"
+                type="number"
+                value=${quantity}
+                min="1"
+              />
+            </div>
+          </div>
+          <div class="col-3 text-end">
+            <div class="item-price">$ ${priceTotal}.00</div>
+            <i class="fa-regular fa-trash-can pt-4"></i>
+          </div>
+        </div>
+      </div>`;
+    cartItems.push(newItem);
+    updateCart();
+    saveCartToLocalStorage();
+  });
+});
+
+window.addEventListener("load", function () {
+  updateCart();
 });
